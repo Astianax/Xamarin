292176ea545735ff85d842feace3746ea074920b
Astianax <alyahir09@gmail.com>
2017-06-25 20:13:05 -0400

    First Commit :D

--- RescueMe/RescueMe.Android/MainActivity.cs
+++ RescueMe/RescueMe.Android/MainActivity.cs
@@ -9,27 +9,22 @@ using Android.OS;
 
 namespace RescueMe.Droid
 {
-	[Activity (Label = "RescueMe.Android", MainLauncher = true, Icon = "@drawable/icon")]
-	public class MainActivity : Activity
-	{
-		int count = 1;
+    [Activity(Label = "RescueMe.Android", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
 
-		protected override void OnCreate (Bundle bundle)
-		{
-			base.OnCreate (bundle);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Login);
 
-			// Set our view from the "main" layout resource
-			SetContentView (Resource.Layout.Main);
+            //String webLinkText = '< a href = "https://prativas.files.wordpress.com/2013/05/screenshot-mozilla-firefox-private-browsing.png" >< img src = "https://prativas.files.wordpress.com/2013/05/screenshot-mozilla-firefox-private-browsing.png" alt = "Screenshot-Mozilla Firefox (Private Browsing)" width = "293" height = "254" class="alignnone size-full wp-image-291" /></a> ';
+                TextView linkTextView = (TextView) FindViewById(Resource.Id.textView1);
+              //  holder.profileFeedTitle.setText(Html.fromHtml(webLinkText))); 
 
-			// Get our button from the layout resource,
-			// and attach an event to it
-			Button button = FindViewById<Button> (Resource.Id.myButton);
-			
-			button.Click += delegate {
-				button.Text = string.Format ("{0} clicks!", count++);
-			};
-		}
-	}
+        }
+    }
 }
 
 
--- RescueMe/RescueMe.Android/Resources/Resource.designer.cs
+++ RescueMe/RescueMe.Android/Resources/Resource.designer.cs
@@ -41,6 +41,31 @@ namespace RescueMe.Droid
 			}
 		}
 		
+		public partial class Color
+		{
+			
+			// aapt resource value: 0x7f050000
+			public const int my_blue = 2131034112;
+			
+			// aapt resource value: 0x7f050003
+			public const int my_gray = 2131034115;
+			
+			// aapt resource value: 0x7f050001
+			public const int my_green = 2131034113;
+			
+			// aapt resource value: 0x7f050002
+			public const int my_purple = 2131034114;
+			
+			static Color()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Color()
+			{
+			}
+		}
+		
 		public partial class Drawable
 		{
 			
@@ -60,8 +85,29 @@ namespace RescueMe.Droid
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050000
-			public const int myButton = 2131034112;
+			// aapt resource value: 0x7f070003
+			public const int button1 = 2131165187;
+			
+			// aapt resource value: 0x7f070002
+			public const int checkbox = 2131165186;
+			
+			// aapt resource value: 0x7f070000
+			public const int editText1 = 2131165184;
+			
+			// aapt resource value: 0x7f070001
+			public const int editText2 = 2131165185;
+			
+			// aapt resource value: 0x7f070007
+			public const int myButton = 2131165191;
+			
+			// aapt resource value: 0x7f070004
+			public const int textView1 = 2131165188;
+			
+			// aapt resource value: 0x7f070005
+			public const int textView2 = 2131165189;
+			
+			// aapt resource value: 0x7f070006
+			public const int textView3 = 2131165190;
 			
 			static Id()
 			{
@@ -77,7 +123,10 @@ namespace RescueMe.Droid
 		{
 			
 			// aapt resource value: 0x7f030000
-			public const int Main = 2130903040;
+			public const int Login = 2130903040;
+			
+			// aapt resource value: 0x7f030001
+			public const int Main = 2130903041;
 			
 			static Layout()
 			{
@@ -104,6 +153,22 @@ namespace RescueMe.Droid
 			}
 			
 			private String()
+			{
+			}
+		}
+		
+		public partial class Style
+		{
+			
+			// aapt resource value: 0x7f060000
+			public const int MyCustomTheme = 2131099648;
+			
+			static Style()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Style()
 			{
 			}
 		}
